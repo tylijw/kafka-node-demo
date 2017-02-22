bed9b2327a70edf9f8e2adadfa33437b65917b8a
tylijw <18686762192@163.com>
2017-02-22 04:12:44 -0500

    fixed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var kafka1 = require('./routes/kafka1');
 var app = express();
 
 // view engine setup
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/kafka', require('./routes/kafka'));
-
+app.use('/kafka1',kafka1);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/kafka.js
+++ routes/kafka.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 var kafka = require('kafka-node');
 var Producer = kafka.Producer;
-var client = new kafka.Client();
+//var client = new kafka.Client('10.194.1.2:9092');
 //client.on('error', function(error){console.log(error);})
-
+//var client = new kafka.Client('localhost:9092');
 router.get('/', function(req, res, next) {
+var client = new kafka.Client('10.194.1.2:2181');
     var producer = new Producer(client);
     var km = new kafka.KeyedMessage('key', 'message')
     var payloads = [{
